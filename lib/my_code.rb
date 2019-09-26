40de5ba5cd553887deeeb6d066848bfdbc603678
Emily Haynes <ehayne00@hotmail.com>
2019-09-26 20:36:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  i = 0
+  yield(while i < array.length do
+    new_array.push(array[i]*-1)
+    i+=1
+  end
+  return new_array
+end
+
+
+
+
+
+def my_reduce(array)
+  counter = 0
+  while counter < array.size do
+   if array[counter] === true
+   
+   return true
+   end
+   counter += 1
+end
+ return false
+ end
